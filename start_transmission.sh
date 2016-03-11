4b58fc022a89a3bef09a7e5860c1e07a3bbe189c
Dave Van Dijck <davevandijck@fastmail.fm>
2016-03-11 16:58:54 -0500

    transmission script

---
+++ start_transmission.sh
@@ -0,0 +1,1 @@
+transmission-daemon -b -ep -c /mnt/nfs/qnap/Download/torrent/ --incomplete-dir /mnt/nfs/qnap/Download/torrent/incomplete -w /mnt/nfs/qnap/Download/torrent/complete -e /mnt/nfs/qnap/Download/torrent/transmission.log
