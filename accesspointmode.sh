26a3ac0f6e1ba496bac14c7ec0b1cfaf5bf62a51
Dave Van Dijck <davevandijck@fastmail.fm>
2016-03-11 16:55:29 -0500

    bpro changes

---
+++ accesspointmode.sh
@@ -0,0 +1,6 @@
+hostapd -B /etc/hostapd/hostapd.conf
+ifconfig wlan0 172.16.0.1
+echo 1 > /proc/sys/net/ipv4/ip_forward
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
